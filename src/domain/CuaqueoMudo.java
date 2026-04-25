2d5cc268cc9efa6a814f4cd6a9d49d8eb94dfa59
Josep Pereda <jperedaf1@upao.edu.pe>
2026-04-25 11:13:20 -0500

    Implementacion de CuaqueoMudo

---
+++ src/domain/CuaqueoMudo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author sistemas
+ */
+public class CuaqueoMudo implements Cuaqueable {
+
+    @Override
+    public void cuaquear() {
+        System.out.println("<<Silencio>>");
+    }
+    
+}
