0634e337d5038731dd5a5514ca9d78a8ed159831
Josep Pereda <jperedaf1@upao.edu.pe>
2026-04-25 11:34:24 -0500

    Implementacion de la clase VuelaConAlas de la interface Volable

--- src/domain/Chirreo.java
+++ src/domain/Chirreo.java
@@ -5,7 +5,7 @@ public class Chirreo implements Cuaqueable {
     @Override
     public void cuaquear() {
         System.out.println("Chirr Chirr");
-        
+
     }
 
 }
---
+++ src/domain/VuelaConAlas.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class VuelaConAlas implements Volable {
+
+    @Override
+    public void volar() {
+        System.out.println("Puedo volar con mis alas");
+    }
+}
